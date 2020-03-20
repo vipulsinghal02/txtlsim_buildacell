582fbe3bff5c0de05687bb8abda22dd0f9d8a120
Vipul Singhal <vipulsinghal02@gmail.com>
2020-03-19 17:53:36 -0700

    Slight edits to the readme file

--- docs/index.m
+++ docs/index.m
@@ -12,7 +12,9 @@
 % Tutorial III> shows how to set up the incoherent feedforward loop, 
 % how to set model parameters, and customize the part files. 
 % 
-%
+% The first of these can be opened by typing ``edit tutorial.m'' into the command 
+% line. You can also use the MATLAB publisher to publish this file. 
+% 
 % Vipul Singhal, 18 Mar 2020
 %
 % In this tutorial, we explore and demonstrate some of the finer points of
